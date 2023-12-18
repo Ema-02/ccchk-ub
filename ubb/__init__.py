3325ac45bf591f544476c6aa1bac72baee5757cb
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2023-12-18 12:57:24 +0300

    Update __init__.py

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -11,10 +11,10 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 LOGS = logging.getLogger(__name__)
 
 CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
-API_ID = int(os.getenv('23337074', CONFIG['API_ID']))
-API_HASH = os.getenv('2e0ea05d6d6bfd6708fa51f54f36baf7', CONFIG['API_HASH'])
-DUMP_ID = os.getenv('-1002030176371', CONFIG['DUMP_ID'])
-STRING_SESSION = os.getenv('1BJWap1sBu0bj45Z6VmvsvxCco7pzm1rObd5eKmycMuMzQIuaLkLiTncN1aFTCHoY5EPHqnol1qOE7Z8TYVi87y35NWZOq-948UNj6IGY3XuqLUQ1qAJ3DVbBvkf8kZEy3_YNxbodGSbNId65AAT2G0m98Sw-hchVj6u9ZbWxX8ta1G-oZx8cpc3aRSmKZilfOeh8nKYoOTk9dZLtCNwjPfOiPCiO1_qw_mCjoyfgM7i_Pkxa8lQFVCAIJ44VE3iPSidxRItM9HxU3zsNMDcJH7q2EShsiVFvnVj0ZfPRzgxTnPtuOKO5rPdsciwje8BcKgnWRyxfU3obF2K9NmioddNeA4fSqIY=', CONFIG['STRING_SESSION'])
+API_ID = int(os.getenv('API_ID', CONFIG['23337074']))
+API_HASH = os.getenv('API_HASH', CONFIG['2e0ea05d6d6bfd6708fa51f54f36baf7'])
+DUMP_ID = os.getenv('DUMP_ID', CONFIG['DUMP_ID'])
+STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['1BJWap1sBu0bj45Z6VmvsvxCco7pzm1rObd5eKmycMuMzQIuaLkLiTncN1aFTCHoY5EPHqnol1qOE7Z8TYVi87y35NWZOq-948UNj6IGY3XuqLUQ1qAJ3DVbBvkf8kZEy3_YNxbodGSbNId65AAT2G0m98Sw-hchVj6u9ZbWxX8ta1G-oZx8cpc3aRSmKZilfOeh8nKYoOTk9dZLtCNwjPfOiPCiO1_qw_mCjoyfgM7i_Pkxa8lQFVCAIJ44VE3iPSidxRItM9HxU3zsNMDcJH7q2EShsiVFvnVj0ZfPRzgxTnPtuOKO5rPdsciwje8BcKgnWRyxfU3obF2K9NmioddNeA4fSqIY='])
 
 Ubot = TelegramClient(StringSession(STRING_SESSION),
                       API_ID,
